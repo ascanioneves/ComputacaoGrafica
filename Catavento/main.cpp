58149e19ae4f3a96f594566eb95f6faf1df4d50d
Ascanio Savio <ascaniosavio97@gmail.com>
2022-09-12 19:19:50 -0300

    Update main.cpp

--- Catavento/main.cpp
+++ Catavento/main.cpp
@@ -1,142 +1,125 @@
+#include <GL/gl.h>
 #include <GL/glut.h>
-#include <cmath>
+#include <bits/stdc++.h>
+using namespace std;
 
-// ALUNO: Ascânio Sávio de Araujo Neves
-// Comandos:
-// Girar para esquerda e direita - "x, c"
-// Aumentar e diminuir velocidade - "v, b"
+#define PI acos(-1)
 
-float pi = 3.14;
-float speed_angle = pi/4;
-float sen_60 = 0.86;
-
-typedef struct Vertex {
-    GLfloat x, y;
-} Vertex;
-
-
-Vertex stick[] = {
-        {-1.5, 0.0},
-        {1.5, 0.0},
-        {-1.5, -50.0},
-        {1.5, -50.0}
-};
-
-Vertex flower[] = {
-        {12.5, 25 * sen_60},
-        {-12.5, 25 * sen_60},
-        {25.0, 0.0},
-        {12.5, 25 * (-sen_60)},
-        {-25.0, 0.0},
-        {-12.5, 25 * (-sen_60)}
-};
-
-
-
-void spinFlower(float angle)
+struct vertex
 {
-    Vertex aux;
-
-    for (int i = 0; i < 6; i += 2) {
-        for (int j = i; j <= i+1; j++) {
-            aux = flower[j];
-            flower[j].x = (aux.x * std::cos(angle)) - (aux.y * std::sin(angle));
-            flower[j].y = (aux.y * std::cos(angle)) + (aux.x * std::sin(angle));
-        }
+    float x, y;
+    vertex(float x, float y) : x(x), y(y) {}
+    void rotate(float a, bool clockwise)
+    {
+        if (!clockwise)
+            a = 360 - a;
+        a = (a * PI) / 180;
+        float new_x = (cos(a) * x) + ((sin(a) * -1) * y);
+        float new_y = (sin(a) * x) + (cos(a) * y);
+        x = new_x;
+        y = new_y;
     }
-
-    glutPostRedisplay();
-}
-
-
-void displayFlower()
+    void translate(float a, float b)
+    {
+        x -= a;
+        y -= b;
+    }
+};
+struct color
 {
-    for (int i = 0; i < 6; i += 2) {
+    float r, g, b;
+    color(float r, float g, float b) : r(r), g(g), b(b) {}
+};
+struct triangle
+{
+    color c;
+    vertex v1, v2, v3;
+    triangle(color c, vertex v1, vertex v2, vertex v3) : c(c), v1(v1), v2(v2), v3(v3) {}
+    void draw()
+    {
+        glColor3f(c.r, c.g, c.b);
         glBegin(GL_TRIANGLES);
-
-        glColor3f(0.0, 1.0, 0.5); // cor da flor de abril
-        glVertex2f(0.0,0.0);
-
-        for (int j = i; j <= i+1; j++) {
-            GLfloat x = flower[j].x;
-            GLfloat y = flower[j].y;
-            glVertex2f(x,y);
-        }
-
+        glVertex2f(v1.x, v1.y);
+        glVertex2f(v2.x, v2.y);
+        glVertex2f(v3.x, v3.y);
         glEnd();
     }
-}
-
-
-void displayStick()
-{
-    for (int i=0; i<2; i++) {
-        glBegin(GL_TRIANGLES);
-
-        glColor3f(1.0, 0.5, 0.0); //cor do graveto
-
-        for (int j = i; j <= i+2; j++) {
-            GLfloat x = stick[j].x;
-            GLfloat y = stick[j].y;
-            glVertex2i(x, y);
-        }
-
-        glEnd();
+    void rotate(float ang, bool clockwise)
+    {
+        v2.translate(v1.x, v1.y);
+        v3.translate(v1.x, v1.y);
+        v2.rotate(ang, clockwise);
+        v3.rotate(ang, clockwise);
+        v2.translate(-v1.x, -v1.y);
+        v3.translate(-v1.x, -v1.y);
     }
-}
+};
 
+vector<triangle> v;
 
-void keyboard(unsigned char key, int x, int y)
+void create_triangles()
 {
-    float change;
-
-    switch(key) {
-        case 'x':
-            spinFlower(speed_angle);
-            break;
-        case 'c':
-            spinFlower(-speed_angle);
-            break;
-        case 'v':
-            change = speed_angle + (1.0f * pi/16);
-            speed_angle = change;
-            break;
-        case 'b':
-            change = speed_angle - (1.0f * pi/16);
-            speed_angle = change;
-            break;
-        default:
-            break;
-    }
+    v.clear();
+    v.push_back({color(1.0, 1.0, 1.0), vertex(250.0f, 250.0f), vertex(230.0f, 200.0f), vertex(270.0f, 200.0f)});
+    v.push_back({color(1.0, 0.0, 0.0), vertex(250.0f, 250.0f), vertex(300.0f, 230.0f), vertex(300.0f, 270.0f)});
+    v.push_back({color(0.0, 1.0, 0.0), vertex(250.0f, 250.0f), vertex(230.0f, 300.0f), vertex(270.0f, 300.0f)});
+    v.push_back({color(0.0, 0.0, 1.0), vertex(250.0f, 250.0f), vertex(200.0f, 270.0f), vertex(200.0f, 230.0f)});
 }
-
-// Renderização
+void draw_base()
+{
+    glColor3f(1.0, 1.0, .0);
+    glBegin(GL_POLYGON);
+    glVertex3f(249.0f, 250.0f, -1.0f);
+    glVertex3f(251.0f, 250.0f, -1.0f);
+    glVertex3f(249.0f, 100.0f, -1.0f);
+    glVertex3f(251.0f, 100.0f, -1.0f);
+    glEnd();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    displayStick();
-    displayFlower();
-    glutSwapBuffers();
+    draw_base();
+    for (auto &i : v)
+    {
+        i.draw();
+    }
+    glFlush();
 }
-
+void keyboard(unsigned char key, int x, int y)
+{
+    if (key == 's' || key == 'S')
+    {
+        for (auto &i : v)
+        {
+            i.rotate(30, true);
+        }
+    }
+    else if (key == 'a' || key == 'A')
+    {
+        for (auto &i : v)
+        {
+            i.rotate(30, false);
+        }
+    }
+    glutPostRedisplay();
+}
+void init()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(0.0, 500.0, 0.0, 400.0, -1.0, 1.0);
+}
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-
-    glutInitWindowSize(640, 480);
-    glutCreateWindow("Flor de abril - Catavento");
-
-    glClearColor(0.0, 0.05, 0.10, 1.0);
-    glOrtho(-100.0, 100.0, -100.0, 100.0, 1.0, -1.0);
-    glutReshapeWindow(640, 480);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 400);
+    glutInitWindowPosition(200, 200);
+    glutCreateWindow("Flor de Abril");
+    create_triangles();
+    init();
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutMainLoop();
-
     return 0;
 }
