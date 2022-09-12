22571cf3217761d08e69a07edcaa09814f22bad9
Ascanio Savio <ascaniosavio97@gmail.com>
2022-09-12 19:23:55 -0300

    Update main.cpp

--- Catavento/main.cpp
+++ Catavento/main.cpp
@@ -117,7 +117,7 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 400);
     glutInitWindowPosition(200, 200);
-    glutCreateWindow("Flor de Abril");
+    glutCreateWindow("Flor de Abril - Catavento");
     create_triangles();
     init();
     glutDisplayFunc(display);
