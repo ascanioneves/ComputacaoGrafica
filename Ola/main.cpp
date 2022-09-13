0b0bc807cd45a630008de7e52d6462b12411bc04
Ascanio Savio <ascaniosavio97@gmail.com>
2022-09-12 23:24:19 -0300

    Update main.cpp

--- Ola/main.cpp
+++ Ola/main.cpp
@@ -1,125 +1,139 @@
-#include <GL/gl.h>
 #include <GL/glut.h>
-#include <bits/stdc++.h>
+#include <iostream>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
 using namespace std;
+const pair<int, int> SQUARE1 = make_pair(4, 3);
+const pair<int, int> SQUARE2 = make_pair(2, 1);
+const pair<int, int> SQUARE3 = make_pair(0, -1);
+const pair<int, int> SQUARE4 = make_pair(-2, -3);
+const pair<int, int> SQUARE5 = make_pair(-4, -5);
 
-#define PI acos(-1)
+/* COMMANDS:
+  UP: "q w e r t"
+  DOWN: "a s d f g"
+ */
 
-struct vertex
-{
-  float x, y;
-  vertex(float x, float y) : x(x), y(y) {}
-  void rotate(float a, bool clockwise)
-  {
-    if (!clockwise)
-      a = 360 - a;
-    a = (a * PI) / 180;
-    float new_x = (cos(a) * x) + ((sin(a) * -1) * y);
-    float new_y = (sin(a) * x) + (cos(a) * y);
-    x = new_x;
-    y = new_y;
-  }
-  void translate(float a, float b)
-  {
-    x -= a;
-    y -= b;
-  }
-};
-struct color
-{
-  float r, g, b;
-  color(float r, float g, float b) : r(r), g(g), b(b) {}
-};
-struct triangle
-{
-  color c;
-  vertex v1, v2, v3;
-  triangle(color c, vertex v1, vertex v2, vertex v3) : c(c), v1(v1), v2(v2), v3(v3) {}
-  void draw()
-  {
-    glColor3f(c.r, c.g, c.b);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(v1.x, v1.y);
-    glVertex2f(v2.x, v2.y);
-    glVertex2f(v3.x, v3.y);
-    glEnd();
-  }
-  void rotate(float ang, bool clockwise)
-  {
-    v2.translate(v1.x, v1.y);
-    v3.translate(v1.x, v1.y);
-    v2.rotate(ang, clockwise);
-    v3.rotate(ang, clockwise);
-    v2.translate(-v1.x, -v1.y);
-    v3.translate(-v1.x, -v1.y);
-  }
+static GLfloat v[10]={ // Square coordinates (Y-Axis)
+        0,  2,
+        0,  2,
+        0,  2,
+        0,  2,
+        0,  2
 };
 
-vector<triangle> v;
-
-void create_triangles()
-{
-  v.clear();
-  v.push_back({color(1.0, 1.0, 1.0), vertex(250.0f, 250.0f), vertex(230.0f, 200.0f), vertex(270.0f, 200.0f)});
-  v.push_back({color(1.0, 0.0, 0.0), vertex(250.0f, 250.0f), vertex(300.0f, 230.0f), vertex(300.0f, 270.0f)});
-  v.push_back({color(0.0, 1.0, 0.0), vertex(250.0f, 250.0f), vertex(230.0f, 300.0f), vertex(270.0f, 300.0f)});
-  v.push_back({color(0.0, 0.0, 1.0), vertex(250.0f, 250.0f), vertex(200.0f, 270.0f), vertex(200.0f, 230.0f)});
-}
-void draw_base()
-{
-  glColor3f(1.0, 1.0, .0);
-  glBegin(GL_POLYGON);
-  glVertex3f(249.0f, 250.0f, -1.0f);
-  glVertex3f(251.0f, 250.0f, -1.0f);
-  glVertex3f(249.0f, 100.0f, -1.0f);
-  glVertex3f(251.0f, 100.0f, -1.0f);
-  glEnd();
-}
+// Rendering squares
 void display()
 {
-  glClear(GL_COLOR_BUFFER_BIT);
-  draw_base();
-  for (auto &i : v)
-  {
-    i.draw();
-  }
-  glFlush();
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_QUADS);
+    glColor3f(0.3, 0.3, 0.6);
+    glVertex2f(SQUARE1.first, v[0]);
+    glVertex2f(SQUARE1.second, v[0]);
+    glVertex2f(SQUARE1.second,v[1]);
+    glVertex2f(SQUARE1.first, v[1]);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.4, 0.5, 0.0);
+    glVertex2f(SQUARE2.first, v[2]);
+    glVertex2f(SQUARE2.second, v[2]);
+    glVertex2f(SQUARE2.second,v[3]);
+    glVertex2f(SQUARE2.first, v[3]);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.1, 0.5, 0.4);
+    glVertex2f(SQUARE3.first, v[4]);
+    glVertex2f(SQUARE3.second, v[4]);
+    glVertex2f(SQUARE3.second,v[5]);
+    glVertex2f(SQUARE3.first, v[5]);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.5, 0.1, 0.3);
+    glVertex2f(SQUARE4.first, v[6]);
+    glVertex2f(SQUARE4.second, v[6]);
+    glVertex2f(SQUARE4.second,v[7]);
+    glVertex2f(SQUARE4.first, v[7]);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3f(0.8, 0.3, 0.1);
+    glVertex2f(SQUARE5.first, v[8]);
+    glVertex2f(SQUARE5.second, v[8]);
+    glVertex2f(SQUARE5.second,v[9]);
+    glVertex2f(SQUARE5.first, v[9]);
+    glEnd();
+
+    glFlush();
 }
-void keyboard(unsigned char key, int x, int y)
-{
-  if (key == 's' || key == 'S')
-  {
-    for (auto &i : v)
-    {
-      i.rotate(30, true);
+
+// Changing square positions
+void keyboard(unsigned char key, int x, int y) {
+    if (key == 't') {
+        v[0] += 2;
+        v[1] += 2;
+        display();
+    } else if (key == 'r') {
+        v[2] += 2;
+        v[3] += 2;
+        display();
+    } else if (key == 'e') {
+        v[4] += 2;
+        v[5] += 2;
+        display();
+    } else if (key == 'w') {
+        v[6] += 2;
+        v[7] += 2;
+        display();
+    } else if (key == 'q') {
+        v[8] += 2;
+        v[9] += 2;
+        display();
+    } else if (key == 'g') {
+        v[0] -= 2;
+        v[1] -= 2;
+        display();
+    } else if (key == 'f') {
+        v[2] -= 2;
+        v[3] -= 2;
+        display();
+    } else if (key == 'd') {
+        v[4] -= 2;
+        v[5] -= 2;
+        display();
+    } else if (key == 's') {
+        v[6] -= 2;
+        v[7] -= 2;
+        display();
+    } else if (key == 'a') {
+        v[8] -= 2;
+        v[9] -= 2;
+        display();
     }
-  }
-  else if (key == 'a' || key == 'A')
-  {
-    for (auto &i : v)
-    {
-      i.rotate(30, false);
-    }
-  }
-  glutPostRedisplay();
 }
-void init()
-{
-  glClearColor(0.0, 0.0, 0.0, 0.0);
-  glMatrixMode(GL_PROJECTION);
-  glOrtho(0.0, 500.0, 0.0, 400.0, -1.0, 1.0);
-}
+
 int main(int argc, char **argv)
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(500, 400);
-  glutInitWindowPosition(200, 200);
-  glutCreateWindow("Flor de Abril");
-  create_triangles();
-  init();
-  glutDisplayFunc(display);
-  glutKeyboardFunc(keyboard);
-  glutMainLoop();
-  return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode ( GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow("CG - Ola");
+
+    glClearColor(0.05, 0.05, 0.10, 1.0); // background color
+    glOrtho(-10.0, 10.0, -10.0, 10.0, 1.0, -1.0);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+
+    return 0;
 }
